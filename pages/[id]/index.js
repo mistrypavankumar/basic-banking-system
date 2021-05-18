f5c31fc5cdd88c690de562b8f65bbbc4f7a9d46f
mg143pavankumar <pavansharma.mg143@gmail.com>
2021-05-19 01:19:56 +0530

    updated

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -1,5 +1,60 @@
-const transection = ({ data }) => {
-  console.log(data);
+import { useState } from "react";
+
+const transection = ({ data, users }) => {
+  const [isSending, setIsSending] = useState(false);
+  const [form, setForm] = useState({
+    balance: "",
+    senderName: "",
+    receiverName: "",
+    transectionId: "",
+  });
+  let formBalance = parseInt(form.balance);
+  let currentUserBalance = data.balance;
+  let receiverBalance = 0;
+
+  const createTransectionHistory = async () => {
+    try {
+      const res = await fetch("http://localhost:3000/api/transection", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(form),
+      });
+    } catch (error) {}
+  };
+
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSending = (e) => {
+    e.preventDefault();
+    validate();
+
+    if (formBalance > 0 && formBalance < data.balance) {
+      currentUserBalance = currentUserBalance - parseInt(form.balance);
+    }
+    console.log(currentUserBalance);
+
+    setIsSending(true);
+  };
+
+  const validate = () => {
+    console.log(formBalance);
+    if (!form.balance) {
+      alert("Please enter amount");
+    }
+
+    if (formBalance > data.balance) {
+      alert("Insufficent Balance");
+    }
+  };
+  // console.log(users);
   return (
     <div className="banner__container" style={{ height: "60vh" }}>
       <div className="content" style={{ marginTop: "8rem" }}>
@@ -30,20 +85,34 @@ const transection = ({ data }) => {
                 Select Receiver
               </option>
 
-              <option value=""></option>
+              {users.map((item, key) => {
+                return (
+                  <>
+                    {data.userId != item.userId && (
+                      <option value={item.userId}>{item.name}</option>
+                    )}
+                  </>
+                );
+              })}
             </select>
 
             <input
               type="number"
               placeholder="Enter your amount"
               id="amount"
-              name="amount"
-              min="0"
+              name="balance"
+              value={form.balance}
+              onChange={handleChange}
               required
             />
           </div>
           <div className="transectionField">
-            <button type="submit" name="submit" id="btn">
+            <button
+              onClick={handleSending}
+              type="submit"
+              name="submit"
+              id="btn"
+            >
               Proceed
             </button>
           </div>
@@ -59,9 +128,12 @@ export async function getServerSideProps({ query: { id } }) {
   const res = await fetch(`http://localhost:3000/api/bankingInfo/${id}`);
   const { data } = await res.json();
 
+  const res1 = await fetch("http://localhost:3000/api/bankingInfo/");
+  const { users } = await res1.json();
   return {
     props: {
       data,
+      users,
     },
   };
 }
